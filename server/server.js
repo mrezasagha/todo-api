5607b8ab5d9f74e2203f21fa9d63048d152b74d8
Mreza Sagha <mrezasagha@nobody.invalid>
2018-03-07 11:00:44 +0330

    added patch route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var {mongoose} = require('./db/mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
 var {Todo} = require('./models/todo');
@@ -60,6 +61,30 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+//update using patch
+app.patch('/todos/:id', (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text', 'completed']);
+
+   if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+   }
+   if(_.isBoolean(body.completed) && body.completed) {
+       body.completedAt = new Date().getTime();
+   } else {
+       body.completed = false ;
+       body.completedAt = null;
+   }
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+       if(!todo) {
+           return res.status(404).send();
+       }
+       res.send({todo});
+   }).catch((e) => {
+       return res.status(400).send(e);
+   });
 });
 
 app.listen(port, () => {
